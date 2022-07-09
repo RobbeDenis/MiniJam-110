4f4a8f9d8687651b91883ec083f2d09bc8f9d736
RobbeDenis <robbe.denis@gmail.com>
2022-07-10 00:05:22 +0200

    Shield now recalls

--- Tonatiuh/Assets/Scripts/Shield.cs
+++ Tonatiuh/Assets/Scripts/Shield.cs
@@ -4,25 +4,85 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    [Header("General settings")]
+    [SerializeField] private float m_Speed = 25f;
+
+    [Header("Recall settings")]
+    [SerializeField] private float m_ReturnMultiplier = 0.1f;
+    [SerializeField] private float m_MinReturnSpeed = 2f;
+
+    private ShieldControl m_Controller;
+
     private Rigidbody m_Rigidbody;
+    private Transform m_RecallTarget;
+    private bool m_Recalling = false;
+
+    public void SetController(ShieldControl controller)
+    {
+        m_Controller = controller;
+    }
 
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void Start()
+    {
+        float y = -10f;
+        transform.position = new Vector3(0f, y, 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_Recalling)
+        {
+            Vector3 direction = Vector3.Normalize(m_RecallTarget.position - transform.position);
+            transform.forward = direction;
+
+            float distance = Vector3.Distance(m_RecallTarget.position, transform.position);
+
+            distance *= m_ReturnMultiplier;
+
+            if (distance < m_MinReturnSpeed)
+                distance = m_MinReturnSpeed;
+
+            m_Rigidbody.MovePosition(transform.position + direction * distance);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_Recalling && other.gameObject.tag == "Catch")
+        {
+            Debug.Log("Catched");
+            Deactivate();
+            m_Controller.ShieldArrived();
+        }
+    }
+
+
     public void Throw(Vector3 direction)
     {
         transform.forward = direction;
 
         m_Rigidbody.velocity = new Vector3(0f, 0f, 0f);
-        m_Rigidbody.AddForce(direction * 20, ForceMode.Impulse);
+        m_Rigidbody.AddForce(direction * m_Speed, ForceMode.Impulse);
 
         Debug.Log("Throw");
     }
 
     public void Recall(Transform target)
     {
+        m_Recalling = true;
+        m_RecallTarget = target;
         Debug.Log("Recall");
+    }
+
+    public void Deactivate()
+    {
+        m_Recalling = false;
+        float y = -10f;
+        m_Rigidbody.velocity = new Vector3(0f, 0f, 0f);
+        transform.position = new Vector3(0f, y, 0f);
     }
 }
--- Tonatiuh/Assets/Scripts/ShieldControl.cs
+++ Tonatiuh/Assets/Scripts/ShieldControl.cs
@@ -9,13 +9,19 @@ public class ShieldControl : MonoBehaviour
     [Header("Aim settings")]
     [SerializeField] private float m_MaxAimDistance = 100f;
 
+    [Header("Layer masks")]
+    [SerializeField] private LayerMask m_PlayerMask;
+    [SerializeField] private LayerMask m_ShieldMask;
+
     [Header("References")]
     [SerializeField] private Shield m_Shield;
+    [Space(5)]
     [SerializeField] private Transform m_ShieldSocket;
     [SerializeField] private Transform m_ShieldModel;
     [SerializeField] private Transform m_CameraTransform;
 
     private float m_VerticalOffset = 5f;
+    private bool m_Thrown = false;
 
     private PlayerInputActions m_PlayerControls;
     private InputAction m_IThrowShield;
@@ -24,6 +30,7 @@ public class ShieldControl : MonoBehaviour
     private void Awake()
     {
         m_PlayerControls = new PlayerInputActions();
+        m_Shield.SetController(this);
     }
 
     private void OnEnable()
@@ -45,13 +52,17 @@ public class ShieldControl : MonoBehaviour
 
     private void Throw(InputAction.CallbackContext context)
     {
+        if (m_Thrown)
+            return;
+
         m_ShieldModel.localPosition = new Vector3(0f, -m_VerticalOffset, 0f);
         m_Shield.transform.position = m_ShieldSocket.position;
 
         Vector3 direction;
         RaycastHit hit;
-        if(Physics.Raycast(m_CameraTransform.position, m_CameraTransform.forward, out hit, m_MaxAimDistance))
+        if(Physics.Raycast(m_CameraTransform.position + m_CameraTransform.forward * 2f, m_CameraTransform.forward, out hit, m_MaxAimDistance))
         {
+            //if(hit.)
             direction = Vector3.Normalize(hit.point - m_ShieldSocket.position);
         }
         else
@@ -60,12 +71,20 @@ public class ShieldControl : MonoBehaviour
         }
 
         m_Shield.Throw(direction);
+        m_Thrown = true;
     }
 
     private void Recall(InputAction.CallbackContext context)
     {
-        m_ShieldModel.localPosition = new Vector3(0f, 0f, 0f);
+        if (!m_Thrown)
+            return;
 
         m_Shield.Recall(m_ShieldSocket);
+    }
+
+    public void ShieldArrived()
+    {
+        m_Thrown = false;
+        m_ShieldModel.localPosition = new Vector3(0f, 0f, 0f);
     }
 }
