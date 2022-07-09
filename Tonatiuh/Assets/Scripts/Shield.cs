3ed745d9e16c6a01e50aa3203da10a8c557e993f
RobbeDenis <robbe.denis@gmail.com>
2022-07-10 01:47:25 +0200

    Shield bounces of pillars

--- Tonatiuh/Assets/Scripts/Shield.cs
+++ Tonatiuh/Assets/Scripts/Shield.cs
@@ -7,6 +7,9 @@ public class Shield : MonoBehaviour
     [Header("General settings")]
     [SerializeField] private float m_Speed = 25f;
 
+    [Header("Pillar hit")]
+    [SerializeField] private int m_PierceIncrease = 2;
+
     [Header("Recall settings")]
     [SerializeField] private float m_ReturnMultiplier = 0.1f;
     [SerializeField] private float m_MinReturnSpeed = 2f;
@@ -15,7 +18,9 @@ public class Shield : MonoBehaviour
 
     private Rigidbody m_Rigidbody;
     private Transform m_RecallTarget;
+    private int m_EnemyPierceAmount = 0;
     private bool m_Recalling = false;
+    private bool m_Active = false;
 
     public void SetController(ShieldControl controller)
     {
@@ -52,14 +57,59 @@ public class Shield : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (m_Recalling && other.gameObject.tag == "Catch")
+        if (!m_Active)
+            return;
+
+        if (other.gameObject.tag == "CheckPoint" ||
+            other.gameObject.tag == "Player")
+            return;
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("EnemyHit");
+            m_EnemyPierceAmount--;
+
+            if (m_EnemyPierceAmount <= 0)
+            {
+                Recall(m_Controller.GetSocket());
+            }
+        }
+        else if(!m_Recalling && other.gameObject.tag == "Pillar")
+        {
+            Debug.Log("Pillar");
+            m_EnemyPierceAmount += m_PierceIncrease;
+
+            Vector3 thisPos = transform.position;
+            Vector3 otherPos = other.transform.position;
+            thisPos.y = 0f;
+            otherPos.y = 0f;
+
+            Vector3 pillarNorm = Vector3.Normalize(otherPos - thisPos);
+            Vector3 direction = Vector3.Reflect(transform.forward, pillarNorm);
+
+            //direction.y = 0f;
+            //direction.Normalize();
+
+            transform.forward = direction;
+
+            m_Rigidbody.velocity = new Vector3(0f, 0f, 0f);
+            m_Rigidbody.AddForce(direction * m_Speed, ForceMode.Impulse);
+        }
+        else if (m_Recalling && other.gameObject.tag == "Catch")
         {
             Debug.Log("Catched");
             Deactivate();
             m_Controller.ShieldArrived();
         }
+        else if(other.gameObject.tag != "Catch")
+        {
+            if(!m_Recalling)
+            {
+                Debug.Log(other.gameObject.tag + "Hit");
+                Recall(m_Controller.GetSocket());
+            }
+        }
     }
-
 
     public void Throw(Vector3 direction)
     {
@@ -67,6 +117,7 @@ public class Shield : MonoBehaviour
 
         m_Rigidbody.velocity = new Vector3(0f, 0f, 0f);
         m_Rigidbody.AddForce(direction * m_Speed, ForceMode.Impulse);
+        m_Active = true;
 
         Debug.Log("Throw");
     }
@@ -81,6 +132,7 @@ public class Shield : MonoBehaviour
     public void Deactivate()
     {
         m_Recalling = false;
+        m_Active = false;
         float y = -10f;
         m_Rigidbody.velocity = new Vector3(0f, 0f, 0f);
         transform.position = new Vector3(0f, y, 0f);
