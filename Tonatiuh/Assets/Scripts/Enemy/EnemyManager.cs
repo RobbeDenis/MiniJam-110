80f04bee000aa81c1b7b36f70d9c608a2a4e2827
Glenn Quintyn <glenn.quintyn@gmail.com>
2022-07-09 17:55:56 +0200

    Enemy goes to player
    Made enemy prefab and the enemy now moves towards the player to a certain respecting distance

--- Tonatiuh/Assets/Scripts/Enemy/EnemyManager.cs
+++ Tonatiuh/Assets/Scripts/Enemy/EnemyManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    [Header("Player")]
+    [SerializeField] private Transform m_PlayerCameraTransform;
+
+    [Header("EnemyPrefabs")]
+    [SerializeField] private GameObject m_BasicEnemy;
+
+    [Header("Settings")]
+    [SerializeField] private int m_BasicEnemyCount;
+
+    private List<GameObject> m_BasicEnemies = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //looking for player in scene
+        if (m_PlayerCameraTransform == null)
+        {
+            Debug.LogWarning("NO PLAYER WAS GIVING!\nSEARCHING FOR PLAYER IN SCENE");
+
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            if (playerObj)
+                m_PlayerCameraTransform = playerObj.transform;
+            else
+                Debug.LogError("NO PLAYER IN SCENE");
+        }
+
+        for (int count = 0; count < m_BasicEnemyCount; count++)
+        {
+            GameObject tempEnemy = Instantiate(m_BasicEnemy, transform);
+            BasicEnemy basicCmpt = tempEnemy.GetComponent<BasicEnemy>();
+
+            if (basicCmpt)
+            {
+                basicCmpt.m_PlayerTransform = m_PlayerCameraTransform;
+            }
+
+            m_BasicEnemies.Add(tempEnemy);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
